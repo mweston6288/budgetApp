da7a324e3b53e7969c29b173407ba0bb0548a438
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-04-19 14:56:21 -0400

    Wanted to reset everything to be safe.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("client/public"));
+app.use(express.static("client"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
